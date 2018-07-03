a90e22433ef92e354b4269e662cccb0e6899cd37
Mateusz Bielak <mateusz.bielak@skygate.io>
2018-07-03 20:46:54 +0200

    Heroku setup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 const path = require('path');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
@@ -48,6 +50,6 @@ app.get('/about', (request, res) =>
 
 app.get('/bad', (req, res) => res.send({ errorMessage: 'Bad request' }));
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
